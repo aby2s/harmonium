5db4f835841be1baca15deb41308fa382423950a
Zenovkin_N <aby2sz@gmail.com>
2018-05-11 20:36:17 +0300

    version and dependencies

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import find_packages
 
 
 setup(name='boltzmann',
-      version='0.0.1',
+      version='0.0.2',
       description='Framework for building RBM',
       author='Nikolay Zenovkin',
       author_email='aby2sz@gmail.com',
-      url='https://github.com/aby2s/boltzmann',
-      download_url='https://github.com/aby2s/boltzmann',
+      url='https://github.com/aby2s/harmonium',
+      download_url='https://github.com/aby2s/harmonium',
       license='MIT',
       install_requires=['tensorflow','numpy', 'Pillow', 'scikit-learn'],
       packages=find_packages())
